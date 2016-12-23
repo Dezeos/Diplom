8c98d6beb11ad1606653bc1ce35b757151e4f721
Dem <s.werewolf.d@gmail.com>
2016-12-23 22:34:24 +0200

    Первый коммит моего дипломного. Было сделано: -дизайн трёх активностей приложения; -наработки перехода между активностями; -поключение проекта к гит репозиторию(леучше поздно чем еще позже)ж

---
+++ app/src/main/java/comindmytroskoryk/linkedin/ua/diplom/Elements_DB.java
@@ -0,0 +1,65 @@
+package comindmytroskoryk.linkedin.ua.diplom;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class Elements_DB extends AppCompatActivity {
+
+    private static final String TAG = "MyAppPP";
+
+    ImageButton im_btn;
+    int num = 0;
+    String name = "Мы сделаем свой аналог списка. Для начала придумаем данные. Пусть это снова будет штатное расписание с именами работников, должностями и зарплатой. Т.е. каждый пункт нашего списка будет содержать три текстовых не редактируемых поля - name, position, salary. А пункты мы разместим в виде вертикального списка.";
+    String  description = "Мы сделаем свой аналог списка. Для начала придумаем данные. Пусть это снова будет штатное расписание с именами работников, должностями и зарплатой. Т.е. каждый пункт нашего списка будет содержать три текстовых не редактируемых поля - name, position, salary. А пункты мы разместим в виде вертикального списка.Мы сделаем свой аналог списка. Для начала придумаем данные. Пусть это снова будет штатное расписание с именами работников, должностями и зарплатой. Т.е. каждый пункт нашего списка будет содержать три текстовых не редактируемых поля - name, position, salary. А пункты мы разместим в виде вертикального списка.";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elements__db);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.llSEL);
+
+        LayoutInflater ltInflater = getLayoutInflater();
+
+
+        for (int i = 0; i <5 ; i++) {
+            String buttonNAME = "im_btn" + Integer.toString(i);
+
+            View item = ltInflater.inflate(R.layout.example_layout, linearLayout, false);
+            im_btn = (ImageButton) item.findViewById(R.id.imbSECOND);
+            im_btn.setId(i);
+            Log.i(TAG, String.valueOf(im_btn.getId()));
+            TextView tvPOS = (TextView) item.findViewById(R.id.tvPOS);
+            tvPOS.setText(Integer.toString(i+1));
+            TextView tvNAME = (TextView) item.findViewById(R.id.tvNAME);
+            tvNAME.setText(name);
+            TextView tvDESCR = (TextView) item.findViewById(R.id.tvDESCR);
+            tvDESCR.setText(description);
+            item.getLayoutParams().width = LinearLayout.LayoutParams.MATCH_PARENT;
+
+            linearLayout.addView(item);
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/comindmytroskoryk/linkedin/ua/diplom/Redact_Activity.java
@@ -0,0 +1,27 @@
+package comindmytroskoryk.linkedin.ua.diplom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Redact_Activity extends AppCompatActivity {
+
+    TextView tvTITLE ;
+    EditText etDESC ;
+    Button bSAVE;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_redact);
+
+        tvTITLE = (TextView) findViewById(R.id.tvTITLE);
+        etDESC = (EditText) findViewById(R.id.etDESC);
+        bSAVE = (Button) findViewById(R.id.bSAVE);
+
+
+    }
+}
